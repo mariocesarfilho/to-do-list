a91eb1e30330f340e89d3a457d6f64bf05135492
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2025-10-02 14:57:58 -0300

    Refactor routes to simplify task management and set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-  get "tasks/index"
-  get "tasks/show"
-  get "tasks/edit"
-  get "tasks/new"
-  get "task/index"
-  get "task/show"
-  get "task/new"
-  get "task/edit"
+
+  root "tasks#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :tasks
+
+  get 'inicio' => 'tasks#index'
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
