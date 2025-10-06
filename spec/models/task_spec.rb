2c46a6478c660ae8afa8d7df7326f2ed98e5281b
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2025-10-06 09:34:20 -0300

    Refactor Task model specs to use FactoryBot for object creation

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  let(:task) { build(:task) }
+
   it "é válido quando possue título, descrição e status" do
-    task = Task.new(titulo: "Ruby", descricao: "Aprendendo a programar", status: false)
+    # task = Task.new(titulo: "Ruby", descricao: "Aprendendo a programar", status: false)
     expect(task).to be_valid
   end
 
   it "é inválido quando o título é nulo" do
-    task = Task.new(titulo: nil)
-    task.valid?
-    expect(task.errors[:titulo]).to include("can't be blank")
+    
+    task_sem_titulo = build(:task, titulo: nil)
+
+    # Agora não é mais necessário eu criar o meu objeto aqui, pois estou usando o factorybot para isso
+    # task = Task.new(titulo: nil)
+    task_sem_titulo.valid?
+    expect(task_sem_titulo.errors[:titulo]).to include("can't be blank")
   end
 end
